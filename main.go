3c839f10b2a035c7446d015b2acb7d9ef2c5a0d6
Carlos Martinez <carlosmartinez@Mac-mini-de-Carlos.local>
2025-04-17 12:07:31 +0200

    added some devcontaienr configurations

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
-	"os"
+	//"os"
+	"scheduler/manager"
 	"scheduler/task"
 	"scheduler/worker"
-	"strconv"
+	//"strconv"
 	"time"
 
 	"github.com/golang-collections/collections/queue"
@@ -14,11 +15,13 @@ import (
 )
 
 func main() {
-	host := os.Getenv("CUBE_HOST")
-	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
-
+	//host := os.Getenv("CUBE_HOST")
+	//port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+	host := "localhost"
+	port := 5555
 	fmt.Println("Starting Cube Worker")
 
+	// Instanciamos el worker
 	w := worker.Worker{
 		Queue: *queue.New(),
 		Db: make(map[uuid.UUID]*task.Task),
@@ -27,7 +30,43 @@ func main() {
 	go runTasks(&w)
 	// Use in a docker image
 	//go w.CollectStats()
-	api.Start()
+	go api.Start()
+
+	//creamos el manager pasandole el worker en formato slice
+	worker := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.New(worker)
+
+	for i := range 3 {
+		t := task.Task{
+			ID: uuid.New(),
+			Name: fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled,
+			Image: "strm/helloworld-http",
+		}
+		te := task.TaskEvent {
+			ID: uuid.New(),
+			State: task.Running,
+			Task: t,
+		}
+		m.AddTask(te)
+		m.SendWork()
+	}
+
+	go func() {
+		for {
+			fmt.Printf("[Manager] Updating tasks from %d workers\n", len(m.Workers))
+			m.UpdateTasks()
+			time.Sleep(15*time.Second)
+		}
+	}()
+
+	for {
+		for _, t := range m.TaskDb {
+			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
+			time.Sleep(15 * time.Second)
+		}
+	}
+
 }
 
 func runTasks(w *worker.Worker) {
